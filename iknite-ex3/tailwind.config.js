1b0dc75408edeaa33cacf9182e52b6bf88cd64a4
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-11-17 17:46:27 +0100

    Services section, footer, processes sections fully responsive

--- iknite-ex3/tailwind.config.js
+++ iknite-ex3/tailwind.config.js
@@ -6,6 +6,26 @@ module.exports = {
         './src/**/*.{html,js}',
       ],
   theme: {
+    screens: {
+        'xs': '425px',
+        // => @media (min-width: 425px) { ... }
+
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+
     extend: {
         fontFamily: {
             lexend: ['Lexend']
