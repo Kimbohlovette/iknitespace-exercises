182d6434dc7198cc28425758ba02b586eafabc3d
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-11-18 10:57:44 +0100

    Implemented hover effect on footer nav items, social icons. Made the product sales info section fully responsive

--- iknite-ex3/public/script.js
+++ iknite-ex3/public/script.js
@@ -2,6 +2,8 @@
 const menuBtnEl = document.getElementById('menu-btn')
 const dropDownMenuEl = document.getElementById('dropdown-menu')
 const closeMenuBtnEl = document.getElementById('close-menu-btn')
+const emailControlEl = document.getElementById('email-control')
+const emailInputEl = document.getElementById('email')
 
 
 menuBtnEl.addEventListener( 'click', ()=>{
@@ -10,4 +12,14 @@ menuBtnEl.addEventListener( 'click', ()=>{
 
 closeMenuBtnEl.addEventListener('click', ()=>{
     dropDownMenuEl.classList.toggle('hidden')
+})
+
+emailInputEl.addEventListener('focusin', ()=>{
+    emailControlEl.classList.add('ring-4')
+    emailControlEl.classList.add('ring-green-800')
+})
+
+emailInputEl.addEventListener('focusout', ()=>{
+    emailControlEl.classList.remove('ring-4')
+    emailControlEl.classList.remove('ring-green-800')
 })
