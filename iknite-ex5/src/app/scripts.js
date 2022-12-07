17bd5e54ffd69920005185df12b3fcedf49a9a01
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-12-07 23:58:15 +0100

    Implemented the memory functionality in calcualtor app

--- iknite-ex5/src/app/scripts.js
+++ iknite-ex5/src/app/scripts.js
@@ -7,7 +7,9 @@ const bracketEl = document.getElementById('bracket')
 const shiftBtnEl = document.getElementById('shift-btn')
 const shiftEl = document.getElementById('shift')
 const deleteEl = document.getElementById('delete')
-
+const returnMemBtn = document.getElementById('return-mem')
+const addMemBtn = document.getElementById('add-mem')
+const subMemBtn = document.getElementById('sub-mem')
 
 const clearEl = document.getElementById('clear')
 const answerEl = document.getElementById('answer')
@@ -17,6 +19,16 @@ const answerEl = document.getElementById('answer')
 // Get key presses
 
 document.addEventListener('keypress',(e)=>{
+
+    // Return expression result when enter is pressed
+    if(e.code==="Enter"){
+        evaluateMemoryExp()
+        return
+    }
+
+    if(e.key==="Delete"){
+        console.log(e.key)
+    }
     if(!e.code.startsWith('Key')){
         consoleEl.innerText = consoleEl.innerText + e.key
         //console.log(e.key + " " + e.code)
@@ -35,7 +47,7 @@ shiftBtnEl.addEventListener('click',()=> {
 
 numbersEl.addEventListener('click', (event)=>{
     if(event.target.classList.contains('number')){
-        console.log(event.target.innerText)
+        //console.log(event.target.innerText)
         let number = event.target.dataset.target
         consoleEl.innerText = consoleEl.innerText + number
     }
@@ -51,32 +63,14 @@ clearEl.addEventListener('click', ()=>{
 for(const operators of operatorsEl){
     operators.addEventListener('click', (event)=>{
         let operator = event.target.dataset.target
-        console.log(operator)
+        //console.log(operator)
         consoleEl.innerText = consoleEl.innerText + operator
 
     })
 }
 
 equalToEl.addEventListener('click', ()=>{
-    const expression = consoleEl.innerText
-    .split('')
-    if(expression[0]==="0"){
-        expression[0]=""
-    }
-
-    let answer = ""
-
-    try{
-        answer = eval(expression.join(''))  
-    } catch (error){
-        answer = "INVALID EXPRESSION!"
-    }
-    
-    if(!answer){
-        answer= 0
-    }
-    answerEl.innerText = answer
-    consoleEl.innerText = ""
+    evaluateMemoryExp()
 })
 
 
@@ -96,6 +90,20 @@ deleteEl.addEventListener('click', ()=>{
 })
 
 
+// Implement memory functionality
+
+addMemBtn.addEventListener('click', ()=>{
+    addToMemory()
+})
+
+subMemBtn.addEventListener('click', ()=>{
+    subscractFromMemory()
+})
+
+returnMemBtn.addEventListener('click',()=>{
+   returnFromMemory() 
+})
+
 
 // HELPER FUNCTIONS
 
@@ -110,4 +118,55 @@ function backSpace(){
     expression.pop()
     consoleEl.innerText = expression.join('')
     
+}
+
+
+function evaluateMemoryExp(){
+    const expression = consoleEl.innerText
+    .split('')
+    if(expression[0]==="0"){
+        expression[0]=""
+    }
+
+    let answer = ""
+
+    try{
+        answer = eval(expression.join(''))  
+    } catch (error){
+        answer = "INVALID EXPRESSION!"
+    }
+    
+    if(!answer){
+        answer= 0
+    }
+    answerEl.innerText = answer
+    consoleEl.innerText = ""
+
+}
+
+function addToMemory(){
+    let memory = answerEl.innerText
+    if(!memory) return
+    let storedValue = JSON.parse(localStorage.getItem("value"))
+    if(storedValue){
+        let value = parseFloat(storedValue) + parseFloat(memory)
+        localStorage.setItem("value", JSON.stringify(value))
+    }
+    else{
+        localStorage.setItem("value",JSON.stringify(memory))
+    }
+
+
+}
+
+function returnFromMemory(){
+    let storedValue = JSON.parse(localStorage.getItem('value'))
+    consoleEl.innerText = consoleEl.innerText + parseFloat(storedValue)
+}
+
+function subscractFromMemory(){
+    let substracter = answerEl.innerText
+    if(!substracter) return
+    let storedValue = JSON.parse(localStorage.getItem('value'))
+    localStorage.setItem("value", JSON.stringify(parseFloat(storedValue)- parseFloat(substracter)))
 }
