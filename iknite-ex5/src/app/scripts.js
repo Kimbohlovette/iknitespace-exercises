88f56f892dc6404e2abc2f15ff90dabba46a68ea
KIMBOH LOVETTE <kimbohlovette@gmail.com>
2022-12-07 15:23:34 +0100

    Implemented keyboard input for calculator app

--- iknite-ex5/src/app/scripts.js
+++ iknite-ex5/src/app/scripts.js
@@ -7,11 +7,37 @@ const bracketEl = document.getElementById('bracket')
 const shiftBtnEl = document.getElementById('shift-btn')
 const shiftEl = document.getElementById('shift')
 const deleteEl = document.getElementById('delete')
+const memoryEl = document.getElementById('memory')
+
 
 const clearEl = document.getElementById('clear')
 const answerEl = document.getElementById('answer')
 
+// On init, bring the keyboard input field to focus
+
+memoryEl.focus()
+
+// Get key presses
+
+memoryEl.addEventListener('keypress',(e)=>{
+    if(!e.code.startsWith('Key')){
+        consoleEl.innerText = consoleEl.innerText + e.key
+        //console.log(e.key + " " + e.code)
+    }
+})
+
+// Keep the keyboard input field focus
+
+memoryEl.addEventListener('blur', ()=>{
+    memoryEl.focus()
+})
+
+
 var isShift = false
+
+
+
+
 
 
 // toggle shift button
@@ -32,7 +58,7 @@ numbersEl.addEventListener('click', (event)=>{
 
 
 clearEl.addEventListener('click', ()=>{
-    consoleEl.innerText = "0"
+    consoleEl.innerText = ""
     answerEl.innerText = ""
     
 })
@@ -52,11 +78,20 @@ equalToEl.addEventListener('click', ()=>{
     if(expression[0]==="0"){
         expression[0]=""
     }
-    let answer = eval(expression.join(''))
+
+    let answer = ""
+
+    try{
+        answer = eval(expression.join(''))  
+    } catch (error){
+        answer = "INVALID EXPRESSION!"
+    }
+    
     if(!answer){
         answer= 0
     }
     answerEl.innerText = answer
+    consoleEl.innerText = ""
 })
 
 
